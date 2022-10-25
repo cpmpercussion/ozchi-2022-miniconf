8cfb58b5d4c5f4ff44f782fb7b4217ed969bcd37
Charles Martin <cpm@charlesmartin.com.au>
2022-10-25 14:18:22 +1100

    updated small pdf script

--- small-pdfs.sh
+++ small-pdfs.sh
@@ -1,3 +1,13 @@
-for i in *.pdf; 
-do gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=sml-$i $i; 
+#!/bin/bash
+for filename in *.pdf; 
+do 
+ gs \
+ -sDEVICE=pdfwrite \
+ -dCompatibilityLevel=1.4 \
+ -dPDFSETTINGS=/prepress \
+ -dNOPAUSE \
+ -dQUIET \
+ -dBATCH \
+ -o "lbw${filename:0:4}.pdf" \
+ ${filename}; 
 done
